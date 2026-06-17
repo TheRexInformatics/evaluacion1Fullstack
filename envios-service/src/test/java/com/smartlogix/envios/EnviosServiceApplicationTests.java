cc91dca82e4690d23399bb5217866e4b85702a11
DiVilota <di.villota@duocuc.cl>
2026-06-17 09:04:58 -0400

    fix(envios): replace mysql driver with postgresql
    - envios-service pom.xml had mysql-connector-j but application.yml
      expected org.postgresql.Driver, causing 'Cannot load driver class'
    - Replaced with org.postgresql:postgresql runtime dependency
    - Added H2 + test deps for proper test isolation
    - Added @AutoConfigureTestDatabase + H2Dialect to tests

--- envios-service/src/test/java/com/smartlogix/envios/EnviosServiceApplicationTests.java
+++ envios-service/src/test/java/com/smartlogix/envios/EnviosServiceApplicationTests.java
@@ -1,9 +1,13 @@
 package com.smartlogix.envios;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.boot.jdbc.test.autoconfigure.AutoConfigureTestDatabase;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.TestPropertySource;
 
 @SpringBootTest
+@AutoConfigureTestDatabase
+@TestPropertySource(properties = "spring.jpa.properties.hibernate.dialect=org.hibernate.dialect.H2Dialect")
 class EnviosServiceApplicationTests {
 
 	@Test
